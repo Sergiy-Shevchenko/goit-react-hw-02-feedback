4aa3d0fb6074cf16e310b80b7000a0e01e47421f
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-05-02 00:29:41 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import Statistics from "./statistics-task-1/statistics-task-1";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Statistics/>
+
   );
 };
+
